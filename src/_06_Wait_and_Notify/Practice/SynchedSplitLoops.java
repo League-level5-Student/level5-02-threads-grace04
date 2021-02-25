d0f4d9c12719baf705c4d8ca39e9c9bb32d2b1bf
grace04 <gracewlyx@gmail.com>
2021-02-24 19:54:56 -0800

    done

--- src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
+++ src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
@@ -17,18 +17,36 @@ printed in order.
 
 public class SynchedSplitLoops implements Runnable {
 	static int counter = 0;
-	Object lock = new Object();
+	static Object lock = new Object();
 	
 	public static void main(String[] args) {
 		Thread t1 = new Thread(() -> {
-			for(int i = 0; i < 100000; i++) {
-				counter++;
+			synchronized(lock) {
+				for(int i = 0; i < 100000; i++) {
+					counter++;
+					lock.notify();
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		});
 		
 		Thread t2 = new Thread(() -> {
-			for(int i = 0; i < 100000; i++) {
-				System.out.println(counter);
+			synchronized(lock) {
+				for(int i = 0; i < 100000; i++) {
+					System.out.println(counter);
+					lock.notify();
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		});
 		
@@ -43,17 +61,10 @@ public class SynchedSplitLoops implements Runnable {
 		}
 		
 	}
-	
+
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
 		
-		synchronized(lock) {
-			lock.notify();
-			try {
-				lock.wait();
-			} catch (InterruptedException e) {
-				System.out.println("error!");
-			}
-		}
 	}
 }
